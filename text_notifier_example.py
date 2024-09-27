7c94f1f7192e2fd56e1767c9e5bb7e89750e0bed
StatStud <63934764+StatStud@users.noreply.github.com>
2024-09-26 20:15:51 -0400

    Create text_notifier_example.py

---
+++ text_notifier_example.py
@@ -0,0 +1,12 @@
+from text_notifier import init_notifier
+
+# Initialize the notifier at the start of your script
+init_notifier()
+
+lst = [1,2]
+def main():
+    print("running...")
+    print(lst[30])
+
+if __name__ == "__main__":
+    main()
